ad5d6c8a1a070a0545e86e95cba0ed6532bae9b6
Peter <peterlanier@me.com>
2019-05-16 12:09:45 -0600

    Add Card component and use roboHash API to populate images, add robots model

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Graham Bear',
+        username: 'Robo',
+        email: 'sincerly@mymail.com'
+    },
+    {
+        id: 2,
+        name: 'Mr. Nice',
+        username: 'Fifi',
+        email: 'yomail@mymail.com'
+    },
+    {
+        id: 3,
+        name: 'Tiger Patches',
+        username: 'Tigg',
+        email: 'momo@mymail.com'
+    }
+];
