d4975004a40663f91ff87b3ced9fe7f024653bd3
Peter <peterlanier@me.com>
2019-05-24 10:37:17 -0600

    Organize components into folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
+import Scroll from '../components/Scroll';
 
 class App extends React.Component {
     constructor() {
@@ -34,7 +34,9 @@ class App extends React.Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
                     <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
     
             );
